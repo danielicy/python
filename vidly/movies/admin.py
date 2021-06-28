e93fa2a25155bdc4ffaaf07a216a9c8165b994a1
Danielicy <danielicy@hotmail.com>
2021-06-28 18:11:45 +0300

    admin

---
+++ vidly/movies/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Genre, Movie
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie)
