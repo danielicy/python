6fa2786ac33217f44016089d793a879557c309b8
Danielicy <danielicy@hotmail.com>
2021-06-29 08:38:42 +0300

    deploy

--- vidly/vidly/settings.py
+++ vidly/vidly/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0l0ihah(*+&4zxbi35bwmd&144cu7_zdzs$0d52s+_lhr$y(^p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-jedi.herokuapp.com']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
